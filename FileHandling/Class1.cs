903af8b248d46dab1714edf2f6d48919dd33bdb7
SauravTandon <SauravTandon@GEDU-LP389>
2024-01-23 22:02:25 +0530

    Add project files.

---
+++ FileHandling/Class1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.PortableExecutable;
+using System.Threading.Tasks;
+
+
+
+namespace FileHandling
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int ch = 0;
+            char c;
+            do
+            {
+                Console.WriteLine(".....MENU.....");
+                Console.WriteLine("1.Create a New File");
+                Console.WriteLine("2.Add Contents to the File");
+                Console.WriteLine("3.Append Contents to the File");
+                Console.WriteLine("4.Display Contents one by one");
+                Console.WriteLine("5.Display All Contents together");
+                Console.WriteLine("6.Delete");
+                Console.WriteLine("Enter your choice");
+                ch = int.Parse(Console.ReadLine());
+                string path = "C:\\Files\\abc.txt";
+                switch (ch)
+                {
+                    case 1:
+                        {
+                            FileInfo fileinfo = new FileInfo(path);
+                            if (!fileinfo.Exists)
+                            {
+                                FileStream fs = new FileStream(path, FileMode.Create);
+
+                                Console.WriteLine("File Created");
+                                fs.Close();
+                            }
+                            else
+                            {
+                                Console.WriteLine("File exists already");
+                            }
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            FileInfo file = new FileInfo(path);
+                            if (file.Exists)
+                            {
+                                FileStream fs1 = file.Open(FileMode.Open, FileAccess.Write);
+                                StreamWriter sw = new StreamWriter(fs1);
+                                sw.WriteLine(" Writing content to file");
+                                sw.Close();
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("File does not exists");
+                            }
+                            break;
+
+                        }
+                    case 3:
+                        {
+                            FileInfo file = new FileInfo(path);
+                            if (file.Exists)
+                            {
+                                using (StreamWriter sw = File.AppendText(path))
+                                {
+                                    sw.WriteLine("Append contents to file");
+                                    sw.Close();
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("File not exists");
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            FileInfo file = new FileInfo(path);
+                            if (file.Exists)
+                            {
+                                using (StreamReader sr = new StreamReader(path))
+                                {
+                                    string line;
+                                    while ((line = sr.ReadLine()) != null)
+                                    {
+                                        Console.WriteLine(line);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("File not exists");
+                            }
+
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            FileInfo file = new FileInfo(path);
+                            if (file.Exists)
+                            {
+                                string textread = File.ReadAllText(path);
+                                Console.WriteLine(textread);
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("File not Exists");
+                            }
+
+                            break;
+                        }
+                    case 6:
+                        {
+                            DirectoryInfo di = new DirectoryInfo("C:\\Files\\");
+                            foreach (FileInfo file in di.GetFiles())
+                            {
+                                file.Delete();
+                            }
+                            Console.WriteLine("File Deleted!!");
+                            break;
+
+                        }
+                }
+
+
+
+                Console.WriteLine("Wish to continue...(y/n)");
+                c = char.Parse(Console.ReadLine());
+            } while (c == 'y');
+       
+
+    }
+    }
+}
---
+++ FileHandling/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Directory.CreateDirectory("demo");
+
+        Directory.CreateDirectory("demo/demo1");
+        Directory.CreateDirectory("demo/demo2");
+
+        File.WriteAllText("demo/demo1/file1.txt", "This is the content of file1.txt");
+        FileInfo file2 = new FileInfo("demo/demo2/file2.txt");
+        using (StreamWriter sw = file2.CreateText())
+        {
+            sw.WriteLine("This is the content of file2.txt");
+        }
+        File.AppendAllText("demo/demo1/file1.txt", "\nThis is the appended content of file1.txt");
+        //file2.AppendText("demo/demo2/file1.txt", "\nThis is the appended content of file2.txt");
+
+
+        //File.Copy("demo/demo1/file1.txt", "demo/demo2/file2_copy.txt");
+
+
+        Console.WriteLine("Files:");
+        foreach (string file in Directory.GetFiles("demo/demo1"))
+        {
+            Console.WriteLine(file + " created at " + File.GetCreationTime(file));
+        }
+        foreach (string file in Directory.GetFiles("demo/demo2"))
+        {
+            Console.WriteLine(file + " created at " + File.GetCreationTime(file));
+        }
+        Console.WriteLine("\nDirectories:");
+        foreach (string dir in Directory.GetDirectories("demo"))
+        {
+            Console.WriteLine(dir + " created at " + Directory.GetCreationTime(dir));
+        }
+
+
+        foreach (string file in Directory.GetFiles("demo/demo1"))
+        {
+            File.Delete(file);
+        }
+
+        foreach (string dir in Directory.GetDirectories("demo"))
+        {
+           Directory.Delete(dir);
+        }
+
+    }
+}
+
